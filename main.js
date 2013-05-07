d9e2237a80c34d6d79fcbde7f7cdfd87de43a0bb
Austin Packer <packer.austin@gmail.com>
2013-05-07 01:17:44 -0400

    update initial conditions

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function runSystem(ctx, state, r1, r2, numberOfPoints, slices, dx, radius, ctxHe
 	for (r = r1, slice = 1; r < r2 && slice < slices; r += dr, slice++)
 	{
 		//don't plot the first 1000
-		s = state
+		s = state;
 		for (var i = 1; i < 1000; i++)
 		{
 			s = r * (1 - s) * s; // quadratic map
@@ -77,17 +77,9 @@ function drawLabel(label, px, py, ctx)
 function drawLabels(ctx, ctxWidth, ctxHeight, r1, r2, border, scale)
 {
     drawLabel("0.0", (border-35), (ctxHeight-border), ctx);
-	//drawLabel("0.1", (border-35), (ctxHeight-border-scale*0.1), ctx);
-	//drawLabel("0.2", (border-35), (ctxHeight-border-scale*0.2), ctx);
 	drawLabel("0.25", (border-35), (ctxHeight-border-scale*0.25), ctx);
-	//drawLabel("0.3", (border-35), (ctxHeight-border-scale*0.3), ctx);
-	//drawLabel("0.4", (border-35), (ctxHeight-border-scale*0.4), ctx);
 	drawLabel("0.5", (border-35), (ctxHeight-border-scale*0.5), ctx);
-	//drawLabel("0.6", (border-35), (ctxHeight-border-scale*0.6), ctx);
-	//drawLabel("0.7", (border-35), (ctxHeight-border-scale*0.7), ctx);
 	drawLabel("0.75", (border-35), (ctxHeight-border-scale*0.75), ctx);
-	//drawLabel("0.8", (border-35), (ctxHeight-border-scale*0.8), ctx);
-	//drawLabel("0.9", (border-35), (ctxHeight-border-scale*0.9), ctx);
 	drawLabel("1.0", (border-35), (ctxHeight-border-scale), ctx);
 	drawLabel(r1, border, (ctxHeight-10), ctx);
 	drawLabel(r2, ctxWidth-border, (ctxHeight-10), ctx);
@@ -116,13 +108,12 @@ function main()
         var r1 = document.getElementById('R1').value - 0;
 		var r2 = document.getElementById('R2').value - 0;
 		var slices = document.getElementById('slices').value - 0;
-        var scale = 500;
-
+        
         var radius = 0.0001;
         var border = 40; // number of pixels around the graph.
 
         var graphWidth = ctxWidth - (2 * border); // make room for axes and labels.
-        var graphHeight = ctxHeight - (2 * border); // TODO: use this to get scale
+        var graphHeight = ctxHeight - (2 * border);
         var dx = (graphWidth / slices);
 
 		
@@ -131,8 +122,8 @@ function main()
 		{
 			ctx.clearRect(0, 0, ctxWidth, ctxHeight);
 			drawAxes(ctx, ctxWidth, ctxHeight, border);
-			drawLabels(ctx, ctxWidth, ctxHeight, r1, r2, border, scale);
-			runSystem(ctx, state, r1, r2, numberOfPoints, slices, dx, radius, ctxHeight, border, scale);
+			drawLabels(ctx, ctxWidth, ctxHeight, r1, r2, border, graphHeight);
+			runSystem(ctx, state, r1, r2, numberOfPoints, slices, dx, radius, ctxHeight, border, graphHeight);
         }
 		else
 		{
